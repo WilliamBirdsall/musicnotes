2b2db72162cfea522b20c0c68997809e5ad37cbf
Will Birdsall <will@ignition72.com>
2023-03-02 21:15:55 -0500

    Move delete and edit actions for note into note detail

--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -1,31 +1,8 @@
 import { Link } from 'react-router-dom';
-import { useQueryClient, useMutation } from '@tanstack/react-query';
-import {useState} from 'react';
-
-import Icons from '../shared/Icons';
 
 const Note = (props) => {
     const noteLink = `/notes/${props.id}`;
-    const editLink = `/edit/${props.id}`;
 
-    const queryClient = useQueryClient();
-
-    // TODO: Show modal for delete confirmation
-    const deleteNote = useMutation({
-        mutationFn: (event) => {
-            event.preventDefault();
-            localStorage.removeItem(props.id);
-
-            // Invalidate 'notes' query
-            queryClient.invalidateQueries({queryKey: ['notes']});
-        }
-    });
-
-    const [moreOpen, setMoreOpen] = useState(false);
-
-    const toggleMoreOpen = () => {
-        moreOpen ? setMoreOpen(false) : setMoreOpen(true);
-    };
 
     return (
         <div className="note">
@@ -37,21 +14,6 @@ const Note = (props) => {
             <div className="note__meta">
                 {props.genre} &bull; {props.bpm}bpm &bull; {props.noteKey} {props.scale}
             </div>
-            {!moreOpen &&
-                <button onClick={() => toggleMoreOpen()} className="btn note__more-btn">
-                    <Icons.MoreIcon />
-                </button>
-            }
-            {moreOpen &&
-                <>
-                    <Link to={editLink} className="note__edit">
-                        <Icons.EditIcon />
-                    </Link>
-                    <button onClick={deleteNote.mutate} className="btn note__delete">
-                        <Icons.DeleteIcon />
-                    </button>
-                </>
-            }
         </div>
     );
 };
--- src/components/note/NoteDetail.jsx
+++ src/components/note/NoteDetail.jsx
@@ -1,18 +1,38 @@
+import {useState} from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
 
 import { NoteContext } from './NoteContext';
 
-import getNote from '../../queries/getNote';
 import CreateSectionButton from '../section/CreateSectionButton';
+import getNote from '../../queries/getNote';
+import Icons from '../shared/Icons';
 import Section from '../section/Section';
 
 const NoteDetail = (props) => {
+    const queryClient = useQueryClient();
+
+    // TODO: Show modal for delete confirmation
+    const deleteNote = useMutation({
+        mutationFn: (event) => {
+            event.preventDefault();
+            localStorage.removeItem(note.id);
+
+            // Invalidate 'notes' query
+            queryClient.invalidateQueries({queryKey: ['notes']});
+        }
+    });
+
+    const [moreOpen, setMoreOpen] = useState(false);
+
     const { id } = useParams();
 
     const note = useQuery(['note', id], getNote).data;
 
     if(!note) return <>No note found :(</>
+
+    const editLink = `/edit/${note.id}`;
 
     const sections = Object.values(note.sections);
 
@@ -21,6 +41,21 @@ const NoteDetail = (props) => {
             <div className="note-detail">
                 <h1 className="note-detail__heading">
                     {note.title} &ndash; {note.artist}
+                {!moreOpen &&
+                    <button onClick={() => setMoreOpen(true)} className="btn note__more-btn">
+                        <Icons.MoreIcon />
+                    </button>
+                }
+                {moreOpen &&
+                    <>
+                        <Link to={editLink} className="note__edit">
+                            <Icons.EditIcon />
+                        </Link>
+                        <button onClick={deleteNote.mutate} className="btn note__delete">
+                            <Icons.DeleteIcon />
+                        </button>
+                    </>
+                }
                 </h1>
                 <div className="note-detail__meta">
                     {note.genre} &bull; {note.bpm}bpm &bull; {note.noteKey} {note.scale}
